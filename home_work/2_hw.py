26a878250e60cacea5a067ea31f0b3623aaf4c83
Tim <123timcapello@gmail.com>
2025-01-18 15:51:36 +0300

    Second Commmit

--- home_work/2_hw.py
+++ home_work/2_hw.py
@@ -1,19 +1,5 @@
 #Задача 1
-def task_1():
-    a = 10
-    print(a, type(a))
-
-    b = 10.1
-    print(b, type(b))
-
-    c = 'Имя'
-    print(c, type(c))
-
-    d = [1, 5, 6, 4, 3]
-    print(d, type(d))
-
-    e = 'bools'
-    print(bool, 9 > 3)
+def task_1(a: int='3', b: float = '10.1', c: str='Имя', d: list = [1,5,6,4,3], e: bool=True ) :
     return a,b,c,d,e
 print(task_1(), '\n')
 
@@ -27,10 +13,9 @@ print('Числа Фибоначчи',task_2(),'\n')
 
 
 #Задача 3
-def task_3():
-    a=7
-    return a**2
-print(task_3())
+def task_3(a:int) -> int:
+    return a*a
+print(task_3(10))
 
 
 
